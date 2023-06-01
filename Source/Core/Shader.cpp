0f8dbb4910ce79b48c3a92da3e14c0307b2e3f58
Hibyehello <joshuadcox05@gmail.com>
2023-06-01 15:43:43 -0500

    Add support for more types with uniforms

--- Source/Core/Shader.cpp
+++ Source/Core/Shader.cpp
@@ -40,15 +40,27 @@ void Shader::SetUniform1f(const std::string& name, float value) {
 	GLCall(glUniform1f(GetUniformLocation(name), value));
 }
 
+void Shader::SetUniform2f(const std::string& name, float v0, float v1) {
+	GLCall(glUniform2f(GetUniformLocation(name), v0, v1));
+}
+
+void Shader::SetUniform3f(const std::string& name, float v0, float v1, float v2) {
+	GLCall(glUniform3f(GetUniformLocation(name), v0, v1, v2));
+}
+
 void Shader::SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3) {
 	GLCall(glUniform4f(GetUniformLocation(name), v0, v1, v2, v3));
+}
+
+void Shader::SetUniformMat3f(const std::string& name, glm::mat3 matrix) {
+	GLCall(glUniformMatrix3fv(GetUniformLocation(name), 1, GL_FALSE, &matrix[0][0]));
 }
 
 void Shader::SetUniformMat4f(const std::string& name, glm::mat4 matrix) {
 	GLCall(glUniformMatrix4fv(GetUniformLocation(name), 1, GL_FALSE, &matrix[0][0]));
 }
 
-int Shader::GetUniformLocation(const std::string& name) {
+int Shader::GetUniformLocation(const std::string& name) const {
 	if(m_location_cache.find(name) != m_location_cache.end())
 		return m_location_cache[name];
 
--- Source/Core/Shader.h
+++ Source/Core/Shader.h
@@ -9,7 +9,7 @@ private:
 	std::string m_filepath_v;
 	std::string m_filepath_f;
 	uint32_t m_renderer_id;
-	std::unordered_map<std::string, int> m_location_cache;
+	mutable std::unordered_map<std::string, int> m_location_cache;
 public:
 	Shader(const std::string& vfile, const std::string& ffile);
 	~Shader();
@@ -20,11 +20,15 @@ public:
 	// Set Uniforms
 	void SetUniform1i(const std::string& name, int value);
 	void SetUniform1f(const std::string& name, float value);
+	void SetUniform2f(const std::string& name, float v0, float v2);
+	void SetUniform3f(const std::string& name, float v0, float v2, float v3);
 	void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
+	
+	void SetUniformMat3f(const std::string& name, glm::mat3 matrix);
 	void SetUniformMat4f(const std::string& name, glm::mat4 matrix);
 
 private:
-	int GetUniformLocation(const std::string& name);
+	int GetUniformLocation(const std::string& name) const;
 	uint32_t CompileShader(uint32_t type, std::string source);
 	uint32_t CreateShader(const std::string& vertex, const std::string& fragment);
 };
